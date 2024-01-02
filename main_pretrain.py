862f33eee1f5bc953a5bdbc4d1739d567f0b8eaf
Ashley Ferreira <48133864+ashley-ferreira@users.noreply.github.com>
2024-01-02 00:06:29 -0500

    larger run

--- main_pretrain.py
+++ main_pretrain.py
@@ -14,13 +14,13 @@ canfar_dataloader_path = '/arc/home/ashley/SSL/git/dark3d/src/models/training_fr
 cc_dataloader_path = '/home/a4ferrei/projects/rrg-kyi/a4ferrei/github/dark3d/src/models/training_framework/'
 
 canfar_data_path = '/arc/projects/unions/ssl/data/processed/unions-cutouts/ugriz_lsb/10k_per_h5/'
-cc_data_path = '/home/a4ferrei/projects/rrg-kyi/a4ferrei/data/spencer_cutout/valid2/'
+cc_data_path = '/home/a4ferrei/projects/rrg-kyi/a4ferrei/data/spencer_cutout/'
 
 canfar_output_path = '/home/a4ferrei/projects/rrg-kyi/a4ferrei/jobs/'
 cc_output_path = '/home/a4ferrei/projects/rrg-kyi/a4ferrei/'
 
 
-n_cutouts = 5*10000
+n_cutouts = 47*10000
 norm_method = 'min_max' 
 patch_size = 8
 
